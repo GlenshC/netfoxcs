053f0d1e411110322d0841e0c5790060eb9a1ef2
Glensh <glenshc@gmail.com>
2026-08-18 23:42:44 +0800

    add reload history

--- NetfoxCs.cs
+++ NetfoxCs.cs
@@ -1,9 +1,27 @@
 ﻿using Godot;
+using Limitless;
 
 namespace Netfox;
 
 public partial class NetfoxCs: Node
 {
+	public const string NetfoxDir = "res://addons/netfox";
+	public static readonly (string name, string path)[] AutoloadsGd =
+	[
+		("NetworkTime", $"{NetfoxDir}/network-time.gd"),
+		("NetworkTimeSynchronizer", $"{NetfoxDir}/network-time-synchronizer.gd"),
+		("NetworkRollback", $"{NetfoxDir}/rollback/network-rollback.gd"),
+		("NetworkEvents", $"{NetfoxDir}/network-events.gd"),
+		("RollbackSimulationServer", $"{NetfoxDir}/servers/rollback-simulation-server.gd"),
+		("NetworkHistoryServer", $"{NetfoxDir}/servers/network-history-server.gd"),
+		("NetworkSynchronizationServer", $"{NetfoxDir}/servers/network-synchronization-server.gd"),
+		("NetworkIdentityServer", $"{NetfoxDir}/servers/network-identity-server.gd"),
+		("NetworkCommandServer", $"{NetfoxDir}/servers/network-command-server.gd"),
+		("RollbackLivenessServer", $"{NetfoxDir}/servers/rollback-liveness-server.gd"),
+		("InterpolationServer", $"{NetfoxDir}/servers/interpolation-server.gd")
+	];
+
+
 	/// <summary>Reference to the netfox autoload NetworkTime.</summary>
 	public static NetworkTime NetworkTime;
 	/// <summary>Reference to the netfox autoload NetworkTimeSynchronizer.</summary>
@@ -12,7 +30,13 @@ public partial class NetfoxCs: Node
 	public static NetworkRollback NetworkRollback;
 	/// <summary>Reference to the netfox autoload NetworkEvents.</summary>
 	public static NetworkEvents NetworkEvents;
+	/// <summary>Reference to the netfox autoload NetworkCommandServer.</summary>
 	public static NetworkCommandServer NetworkCommandServer;
+	/// <summary>Reference to the netfox autoload NetworkHistoryServer.</summary>
+	public static NetworkHistoryServer NetworkHistoryServer;
+
+	/// <summary>Reload NetworkHistoryServer, this prevents tick rejection from previous game.</summary>
+	public static void Reload() => NetworkHistoryServer?.Reload();
 
 	public override void _EnterTree()
 	{
@@ -21,5 +45,7 @@ public partial class NetfoxCs: Node
 		NetworkRollback = new(GetNode("/root/NetworkRollback"));
 		NetworkEvents = new(GetNode("/root/NetworkEvents"));
 		NetworkCommandServer = new(GetNode("/root/NetworkCommandServer"));
+		NetworkHistoryServer = new(GetNode("/root/NetworkHistoryServer"));
 	}
+
 }
---
+++ servers/history/NetworkHistoryServer.cs
@@ -0,0 +1,24 @@
+﻿using Godot;
+
+namespace Netfox;
+
+public partial class NetworkHistoryServer : NodeWrapper<Node>
+{
+	protected override StringName GdClassName => "_NetworkHistoryServer";
+
+	internal NetworkHistoryServer(Node node) : base(node)
+	{}
+
+	public void Reload() => ObjectInstance.Call(MethodNameGd.Reload);
+
+
+	#region StringName Constants
+
+	static class MethodNameGd
+	{
+		public static readonly StringName
+			Reload = "reload";
+	}
+
+	#endregion
+}
